504995683178b7525cd5e13bed98f0707884165f
LuísVieira808 <aluno22100@ipt.pt>
2021-04-14 11:16:06 +0100

    Correção ao modelo(atributo CaesFK)

--- CriadoresCaes_tA_B/Models/Criadores.cs
+++ CriadoresCaes_tA_B/Models/Criadores.cs
@@ -27,6 +27,7 @@ namespace CriadoresCaes_tA_B.Models {
       /// </summary>
       [Required(ErrorMessage = "O Nome é de preenchimento obrigatório")]
       [StringLength(60, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
+      [RegularExpression("([A-Za-z ]+)+")]
       public string Nome { get; set; }
 
       /// <summary>
@@ -45,17 +46,19 @@ namespace CriadoresCaes_tA_B.Models {
       /// <summary>
       /// Código Postal
       /// </summary>
-      [Required(ErrorMessage = "O Código Postal é de preenchimento obrigatório")]
+      [Required(ErrorMessage = "Deve escrever o {0}")]
       [StringLength(30, MinimumLength = 8, ErrorMessage = "O {0} deve ter entre {2} e {1} caracteres.")]
-      [RegularExpression("[1-9][0-9]{3}-[0-9]{3}")]
+      [RegularExpression("[1-9][0-9]{3}-[0-9]{3}( [a-zA-Z-]+áÁçÇ)+")]
+      [Display(Name ="Código Postal")]
       public string CodPostal { get; set; }
 
       /// <summary>
       /// Telemóvel
       /// </summary>
       [StringLength(14, MinimumLength = 9, ErrorMessage = "O {0} deve ter entre {2} e {1} caracteres.")]
-      [RegularExpression("(00)?([0-9]{2,3})?[1-9][0-9]{8}", ErrorMessage ="Escreva, pf, um nº Telemóvel com 9 algarismos. Se quiser, pode acrescentar o indicativo nacional e o internacional. ")]
-      public string Telemovel { get; set; } // ou se escreve o Telemóvel, ou o Email, ou os dois...
+      [RegularExpression("(00)?([0-9]{2,3})?[1-9][0-9]{8}", ErrorMessage ="Escreva um nº Telemóvel com 9 algarismos. Se quiser, pode acrescentar o indicativo nacional e o internacional. ")]
+        [Display(Name = "Telemóvel")]
+        public string Telemovel { get; set; } // ou se escreve o Telemóvel, ou o Email, ou os dois...
 
       /// <summary>
       /// Email
